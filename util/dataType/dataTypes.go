849ebe49719cb0c540aa6ac7f4bcefcc0b12bea7
JuanHeza <juanehza@hotmail.com>
2023-03-06 13:20:19 -0600

    Cors origin environment variable

--- util/dataType/dataTypes.go
+++ util/dataType/dataTypes.go
@@ -16,22 +16,30 @@ const (
 )
 
 const (
-	Criteria_equal       string = "$eq"
-	Criteria_in          string = "$in"
-	Criteria_grater      string = "$gt"
-	Criteria_less        string = "$lt"
-	Criteria_grater_than string = "$gte"
-	Criteria_less_than   string = "$lte"
-	Criteria_not_equal   string = "$ne"
-	Criteria_and         string = "$and"
-	Criteria_not         string = "$not"
-	Criteria_nor         string = "$nor"
-	Criteria_or          string = "$or"
-	Criteria_exists      string = "$exists"
-	Criteria_type        string = "$type"
-	Criteria_regex       string = "$regex"
-	Criteria_text        string = "$text"
-	Criteria_slice       string = "$slice"
+	Criteria_equal        = "$eq"
+	Criteria_in           = "$in"
+	Criteria_grater       = "$gt"
+	Criteria_less         = "$lt"
+	Criteria_grater_than  = "$gte"
+	Criteria_less_than    = "$lte"
+	Criteria_not_equal    = "$ne"
+	Criteria_and          = "$and"
+	Criteria_not          = "$not"
+	Criteria_nor          = "$nor"
+	Criteria_or           = "$or"
+	Criteria_exists       = "$exists"
+	Criteria_type         = "$type"
+	Criteria_regex        = "$regex"
+	Criteria_text         = "$text"
+	Criteria_slice        = "$slice"
+	Colleccion_cliente    = "clientes"
+	Colleccion_producto   = "productos"
+	Colleccion_tienda     = "tienda"
+	Colleccion_usuario    = "usuarios"
+	Colleccion_pedidos    = "pedidos"
+	Colleccion_inventario = "inventario"
+	Colleccion_ofertas    = "ofertas"
+	Database_Name         = "pruebas"
 )
 
 const (
@@ -44,17 +52,6 @@ const (
 )
 
 const (
-	Colleccion_cliente    = "clientes"
-	Colleccion_producto   = "productos"
-	Colleccion_tienda     = "tienda"
-	Colleccion_usuario    = "usuarios"
-	Colleccion_pedidos    = "pedidos"
-	Colleccion_inventario = "inventario"
-	Colleccion_ofertas    = "ofertas"
-	Database_Name         = "pruebas"
-)
-
-const (
 	Invalid_Status Status = iota
 	Eliminado
 	No_Eliminado
@@ -62,6 +59,7 @@ const (
 
 var (
 	Mongo_uri = os.Getenv("MONGODB_URI")
+	Allow_Origin = os.Getenv("Allow_Origin")
 )
 
 type Criteria struct {
